4c6cc19ddb625302bb8f3a4ec1a27a3e5049900a
xandepena <161486267+xandepena@users.noreply.github.com>
2024-05-07 10:20:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var nota1 = parseFloat(prompt("sua nota de fisica"))
+
+var nota2 = parseFloat(prompt("sua nota de ingles"))
+
+var nota3 = parseFloat(prompt("sua nota de biologia"))
+
+var media = ( nota1, nota2, nota3) / 3
+
+console.log (media)
